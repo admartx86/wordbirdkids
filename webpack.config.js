fda6fffe46b5e1e9dbd36484ecd7a16052c88158
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2024-02-18 17:30:12 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ],
+};
